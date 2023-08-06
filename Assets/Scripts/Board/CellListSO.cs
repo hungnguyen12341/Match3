32140903b30c211ba6a1844dc280ae299281a87d
DESKTOP-2TS1SKP\Gaming <hungqwer1234hn@gmail.com>
2023-08-06 20:15:34 +0700

    *FirstComit

---
+++ Assets/Scripts/Board/CellListSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CellList", menuName = "Custom/Cell List")]
+public class CellListSO : ScriptableObject
+{
+    [System.Serializable]
+    public class Cell
+    {
+        public Sprite sprite;
+      
+        // public int ID;
+    }
+
+    public List<Cell> cellList = new List<Cell>();
+}
